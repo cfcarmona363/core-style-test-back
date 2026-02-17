9f19257eb5ff9dd2308b3451b5c00faefb374345
cfcarmona363 <cfcarmona363@gmail.com>
2026-02-17 14:53:38 -0300

    Remove node_modules and dist directories

--- dist/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * API request/response types for the mail service endpoints.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
